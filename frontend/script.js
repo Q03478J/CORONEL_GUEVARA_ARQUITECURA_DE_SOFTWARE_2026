d60ce8bad4dc995eaa37f3e5a9ba3529cc014f8b
Q03478J <q03478j@upla.edu.pe>
2026-04-08 00:19:40 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,12 @@
+function login() {
+    let user = document.getElementById("usuario").value;
+    let pass = document.getElementById("password").value;
 
+    if (user === "admin" && pass === "123") {
+        window.location.href = "admin.html";
+    } else if (user === "user" && pass === "123") {
+        window.location.href = "usuario.html";
+    } else {
+        window.location.href = "invitado.html";
+    }
+}
